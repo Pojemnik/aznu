754bbcc84007084dba0a6857c07ee272abc63b7b
unknown <andrzej.gauza@outlook.com>
2025-01-05 15:15:22 +0100

    Add web client base

---
+++ aznu-web/src/main/java/org/pojemnik/aznu_web/AznuWebApplication.java
@@ -0,0 +1,13 @@
+package org.pojemnik.aznu_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AznuWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AznuWebApplication.class, args);
+	}
+
+}
---
+++ aznu-web/src/test/java/org/pojemnik/aznu_web/AznuWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pojemnik.aznu_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AznuWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
