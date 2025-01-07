1478411eb71d2022368ad77611cc60e5aa5f6138
unknown <andrzej.gauza@outlook.com>
2025-01-07 16:24:36 +0100

    Contenerize payment server

--- aznu-web/src/main/java/org/pojemnik/ticketweb/controllers/TicketController.java
+++ aznu-web/src/main/java/org/pojemnik/ticketweb/controllers/TicketController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Objects;
-
 @Controller
 public class TicketController
 {
